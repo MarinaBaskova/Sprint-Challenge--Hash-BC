ce8b2c58ad36cb3c5ec63eebce7c11e8a6b7cf6f
Marina Baskova <marinabaskova24@gmail.com>
2019-08-09 09:21:19 -0700

    hashtable first

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,10 +9,14 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
 
+    for i in range(length):
+        x = limit - weights[i]
+        value = hash_table_retrieve(ht, x)
+        if value:
+            return(value, i)
     return None
 
 
